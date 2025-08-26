5c2c5ffadfa987fc699e7d5f0daf53a548f27ffe
HaqueSiam <haquesiam99@gmail.com>
2025-08-27 00:34:28 +0600

    responsive-final

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,17 +2,17 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Frontend Development",
-        s_desc:"Frontend development includes layout, design, and interactivity using technologies like HTML, CSS, and JavaScript. "
+        s_desc:"Frontend development includes layout, design, ... "
     },
     {
         s_no:"02",
         s_name:"Backend Development",
-        s_desc:"Bckend development involves server-side logic, database management, and application programming interfaces (APIs)."
+        s_desc:"Bckend development involves server-side logic, ..."
     },
     {
         s_no:"03",
         s_name:"Database Management",
-        s_desc:"Database management includes designing, implementing, and maintaining databases to store and retrieve data."
+        s_desc:"Database management includes designing, implementing, ..."
     }
 ]
  
