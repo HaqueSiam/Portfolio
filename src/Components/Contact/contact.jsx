1ccee782dd4bb1768b6f23a8e30f5967f57386b8
HaqueSiam <haquesiam99@gmail.com>
2025-08-26 22:03:07 +0600

    responsive

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -5,6 +5,33 @@ import mail_icon from '../../assets/mail_icon.svg'
 import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+    event.preventDefault();
+    //setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "f744715d-8021-44a4-b163-45aea30338b9");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+    
+
+    if (data.success) {
+      alert(data.message)
+      //setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
   return (
     <div id='contact' className="contact">
         <div className="contact-title">
@@ -27,7 +54,7 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <form className='contact-right'>
+            <form onSubmit={onSubmit} className='contact-right'>
                 <label htmlFor="">Your Name</label>
                 <input type="text" placeholder='Enter your name' name='name' />
                 <label htmlFor="">Your Email</label>
--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./hero.css";
 import profile_img from '../../assets/profile_img.png'; // Adjust the path as necessary
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Hero = () => {
   return (
     <div id='home' className="hero">
@@ -8,7 +9,7 @@ const Hero = () => {
       <h1><span>I'm Mohammad Mahmudul Haque Siam,</span> Full-Stack developer</h1>
       <p>I'm a passionate and detail-oriented full stack developer with a strong focus on building modern, user-friendly web applications. I take pride in writing maintainable code and creating meaningful user experiences.</p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
