c2b8dbb7065b56430959ce5de7e1b777d672a1de
HaqueSiam <haque.siam99@gmail.com>
2025-09-08 00:53:53 +0600

    final responsive

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,33 +1,19 @@
+// Footer.jsx
 import React from 'react'
 import './footer.css'
-import footer_logo from '../../assets/logo.svg'
-import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
   return (
     <div className='footer'>
-        <div className="footer-top">
-            <div className="footer-top-left">
-                <img src={footer_logo} alt="" />
-                <p>I’m a highly motivated full-stack web developer with a solid foundation in creating scalable, intuitive, and high-performance web applications.</p>
-            </div>
-            <div className="footer-top-right">
-                <div className="footer-email-input">
-                    <img src={user_icon} alt="" />
-                    <input type="email" placeholder='Enter your email'/>
-                </div>
-                <div className="footer-subscribe">Submit</div>
-            </div>
-        </div>
         <hr />
-            <div className="footer-bottom">
-                <p className="footer-bottom-left">© 2025 Mohammad Mahmudul Haque Siam. All rights rederved.</p>
-                <div className="footer-bottom-right">
-                    <p>Term of Services</p>
-                    <p>Privacy policy</p>
-                    <p>Connect with me</p>
-                </div>
+        <div className="footer-bottom">
+            <div className="footer-bottom-right">
+                <p>Terms of Service</p>
+                <p>Privacy Policy</p>
+                <p>Connect with me</p>
             </div>
+            <p className="footer-bottom-left">© 2025 Mohammad Mahmudul Haque Siam. All rights reserved.</p>
+        </div>
     </div>
   )
 }
